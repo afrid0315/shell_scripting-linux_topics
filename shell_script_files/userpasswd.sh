58d019a7352c71b017b341a93254718901c41c90
Afrid Basha Abdul <afridbashaabdul@gmail.com>
2024-05-17 13:25:58 +0000

    userpasswdFileAdded

---
+++ shell_script_files/userpasswd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -n "Enter your username: "      
+read -s USERNAME                     # read is used for reading the data which we provide in o/p, 
+echo
+echo -n "Enter your password: "      # -s used for securing the data without visibility, 
+read -s PASSWORD                     # USERNAME or PASSWORD is used to act as variable and takes given o/p value.
